01221c0c20d725cf4fccded3f1e0f11157eae122
PopovDmitriy <447039048dp@gmail.com>
2020-03-11 02:37:34 +0300

    v8.0

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
-    function headerToggler () {
-        const header = document.querySelector('js-header');
-        const nav = header.querySelector('[js-nav]');
+    function headerToggler() {
+        const header = document.querySelector('.js-header');
+        const nav = header.querySelector('[data-nav]');
+        const html = document.documentElement;
 
-        function toggleMenu () {
+        const menuBtn = header.querySelector('[data-toggler]');
+
+        function toggleMenu() {
             nav.classList.toggle("is-open");
         }
 
-        toggleMenu();
+        menuBtn.addEventListener('click', function() {
+            toggleMenu();
+            noScroll();
+        });
+
+        function noScroll() {
+            html.classList.toggle('no-scroll');
+        }
     }
     headerToggler();
 })
