2e0a87276d5e904f8836a13a662dae7033f40762
PopovDmitriy <447039048dp@gmail.com>
2020-09-08 18:56:41 +0300

    Update

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ document.addEventListener('DOMContentLoaded', function() {
         const header = document.querySelector('.js-header');
         const nav = header.querySelector('[data-nav]');
         const html = document.documentElement;
-
         const menuBtn = header.querySelector('[data-toggler]');
 
         function toggleMenu() {
@@ -20,4 +19,27 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
     headerToggler();
+
+    // function footerDropDown() {
+    //     const footer = document.querySelector('.js-footer');
+    //     const btn = footer.querySelector('[data-dropbtn]');
+    //     const list = footer.querySelector('[data-droplist]');
+
+    //     function checkStyle() {
+    //         if (list.style.display == 'none') {
+    //             list.style.display = 'block';
+    //             console.log('hello1');
+    //         } else if (list.style.display == 'block') {
+    //             list.style.display = 'none';
+    //             console.log('hello2');
+    //         } else {
+    //             console.log('bie'); 
+    //         }
+    //     }
+
+    //     btn.addEventListener('click', function(){
+    //         checkStyle();
+    //     })
+    // }
+    // footerDropDown();
 })
